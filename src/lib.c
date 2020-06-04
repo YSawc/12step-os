714ed88e8f9c7fbce1fb25ac238ca508c093841c
YSawc <niceage6690@gmail.com>
2020-06-04 22:45:09 +0900

    Add lib for memory.

--- src/lib.c
+++ src/lib.c
@@ -13,3 +13,29 @@ int puts(unsigned char *s) {
     putc(*(s++));
   return 0;
 }
+
+void *memset(void *b, int c, long len) {
+  char *p;
+  for (p = b; len > 0; len--)
+    *(p++) = c;
+  return b;
+}
+
+void *memcpy(void *dst, const void *str, long len) {
+  char *d = dst;
+  const char *s = str;
+  for (long l = len; l > 0; l--)
+    *(d++) = *(s++);
+  return dst;
+}
+
+int memcmp(const void *b1, const void *b2, long len) {
+  const char *p1 = b1, *p2 = b2;
+  for (long l = len; l > 0; l--) {
+    if (*p1 != *p2)
+      return (*p1 > *p2) ? 1 : -1;
+    p1++;
+    p2++;
+  }
+  return 0;
+}
--- src/lib.h
+++ src/lib.h
@@ -3,5 +3,8 @@
 
 int putc(unsigned char c);
 int puts(unsigned char *s);
+void *memset(void *b, int c, long len);
+void *memcpy(void *dst, const void *str, long len);
+int memcmp(const void *b1, const void *b2, long len);
 
 #endif
