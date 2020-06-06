af387ee25bfa8b18c0795112f312861c3075757e
YSawc <niceage6690@gmail.com>
2020-06-06 11:20:56 +0900

    Change to valid rom builtin.

--- src/main.c
+++ src/main.c
@@ -2,12 +2,22 @@
 #include "lib.h"
 #include "serial.h"
 
-int main() {
+void init_static_region() {
+  extern int erodata, data_start, edata, bss_start, ebss;
+
+  // copy rom data to ram data for static variables.
+  _memcpy(&data_start, &erodata, (long)&edata - (long)&data_start);
+  _memset(&bss_start, 0, (long)&ebss - (long)&bss_start);
+
   serial_init(SERIAL_DEFAULT_DEVICE);
+}
+
+int main() {
+  /* init_static_region(); */
 
   _puts((unsigned char *)"Hello world!\n");
 
-  while (1) // roop while
+  while (1)
     ;
 
   return 0;
